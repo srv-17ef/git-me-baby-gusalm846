aa8832db7e6988c2512df6bb83ec870fc63b2533
gusalm846 <54351055+gusalm846@users.noreply.github.com>
2019-08-26 15:06:52 +0200

    hurra

--- test2.php
+++ test2.php
@@ -1,1 +1,12 @@
 <?php
+
+$person = 'Peter';
+$surn = 'Griffin';
+$age = 'Old';
+$iq = '25';
+$weight = 'small car';
+echo '<h1>Family Guy</h1>';
+echo $person.' '.$surn;
+var_dump($iq);
+var_dump($weight);
+var_dump($age);
